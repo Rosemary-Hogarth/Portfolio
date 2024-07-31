8fb1b62abdc9f4859ae8b85e1adf2065e7c8cf17
Rosemary Hogarth <rohogarth@gmail.com>
2024-07-31 09:42:01 +0200

    Re-ordered projects, added lingo ludus

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const weather= document.getElementById("weather");
-let imageIndex = 0;
-const images = ["images/weather.jpeg", "images/weather1.png", "images/weather2.png"];
+const weather = document.getElementById("weather");
+const icecream = document.getElementById("icecream");
+
+let weatherImageIndex = 0;
+const weatherImages = ["images/weather1.png", "images/weather2.png"];
 
 function changeWeatherPic() {
-  weather.src = images[imageIndex];
-  imageIndex = (imageIndex + 1) % images.length;
+    weather.src = weatherImages[weatherImageIndex];
+    weatherImageIndex = (weatherImageIndex + 1) % weatherImages.length;
 }
 
 weather.addEventListener("click", changeWeatherPic);
 
-
-
-
-const icecream = document.getElementById("icecream");
-const iceImages = ["images/icecream2.jpeg", "images/icecream2.png", "images/icecream1.png"];
+let icecreamImageIndex = 0;
+const icecreamImages = ["images/icecream1.png", "images/icecream2.png"];
 
 function changeIcePic() {
-  icecream.src = iceImages[imageIndex];
-  imageIndex = (imageIndex + 1) % images.length;
+    icecream.src = icecreamImages[icecreamImageIndex];
+    icecreamImageIndex = (icecreamImageIndex + 1) % icecreamImages.length;
 }
 
 icecream.addEventListener("click", changeIcePic);
