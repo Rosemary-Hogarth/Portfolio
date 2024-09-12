3f111648fcd1b7441fc994c409fa8b61c2128bbd
Rosemary Hogarth <rohogarth@gmail.com>
2024-09-12 14:22:09 +0200

    Fixed js bug

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const weather = document.getElementById("weather");
-const icecream = document.getElementById("icecream");
+
 
 let weatherImageIndex = 0;
 const weatherImages = ["images/weather1.png", "images/weather2.png"];
 
 function changeWeatherPic() {
+    console.log("weather clicked");
     weather.src = weatherImages[weatherImageIndex];
     weatherImageIndex = (weatherImageIndex + 1) % weatherImages.length;
 }
 
 weather.addEventListener("click", changeWeatherPic);
 
-let icecreamImageIndex = 0;
-const icecreamImages = ["images/icecream1.png", "images/icecream2.png"];
 
-function changeIcePic() {
+
+  const icecream = document.querySelector("#icecream");
+  console.log(icecream);
+
+  let icecreamImageIndex = 0;
+  const icecreamImages = ["images/icecream1.png", "images/icecream2.png"];
+
+  function changeIcePic() {
     icecream.src = icecreamImages[icecreamImageIndex];
     icecreamImageIndex = (icecreamImageIndex + 1) % icecreamImages.length;
-}
+  }
 
-icecream.addEventListener("click", changeIcePic);
+  icecream.addEventListener("click", changeIcePic);
